fc638508a9c8ad66ebaede8c3513b52e0ca1223d
anizmamos <57558984+anizmamos@users.noreply.github.com>
2019-12-26 19:56:13 +0400

    Add files via upload

--- asq.py
+++ asq.py
@@ -1,10 +1,11 @@
 # imports
 import sys
 from random import choice, randint
+from time import sleep
 
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtWidgets import QMainWindow, QLabel
-from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtGui import QPixmap, QIcon, QBrush, QPainter
 
 
 class Opening(QWidget):
@@ -55,28 +56,18 @@ class Menu(QMainWindow):
         # Difficult button1 setup
         self.button_1 = QPushButton('Учебник', self)
         self.button_1.resize(412, 100)
-        self.button_1.move(0, 200)
+        self.button_1.move(0, 300)
         self.button_1.clicked.connect(self.open_handbook)
 
         self.button_2 = QPushButton('Элементы для новичков', self)
         self.button_2.resize(206, 100)
-        self.button_2.move(0, 300)
+        self.button_2.move(0, 400)
         self.button_2.clicked.connect(self.open_easy_elem)
-
-        self.button_3 = QPushButton('O и OH для чайников', self)
-        self.button_3.resize(206, 100)
-        self.button_3.move(206, 300)
-        self.button_3.clicked.connect(self.open_easy_o_oh)
 
         self.button_4 = QPushButton('Элементы для знатоков', self)
         self.button_4.resize(206, 100)
-        self.button_4.move(0, 400)
+        self.button_4.move(206, 400)
         self.button_4.clicked.connect(self.open_hard_elem)
-
-        self.button_5 = QPushButton('O и OH для профи', self)
-        self.button_5.resize(206, 100)
-        self.button_5.move(206, 400)
-        self.button_5.clicked.connect(self.open_easy_o_oh)
 
         self.show()
 
@@ -90,20 +81,10 @@ class Menu(QMainWindow):
         self.easy_Elem_form = EasyElem()
         self.easy_Elem_form.show()
 
-    def open_easy_o_oh(self):
-        # Form changer
-        self.easy_O_OH_form = Wait()
-        self.easy_O_OH_form.show()
-
     def open_hard_elem(self):
         # Form changer
         self.hard_Elem_form = HardElem()
         self.hard_Elem_form.show()
-
-    def open_hard_o_oh(self):
-        # Form changer
-        self.hard_O_OH_form = Wait()
-        self.hard_O_OH_form.show()
 
 
 class Handbook(QWidget):
@@ -233,7 +214,9 @@ class EasyElem(QWidget):
         self.setGeometry(412, 412, 412, 500)
         self.setWindowTitle('Хiмоz@ for punks')
         self.setWindowIcon(QIcon("icon.jpg"))
+        self.setStyleSheet("background-color:" + choice(['red', 'green', 'white', 'yellow', 'grey', 'purple', 'pink']))
 
+        self.score = 0
         self.easyelem = {'H': 'Водород', 'Li': 'Литий', 'Cu': 'Медь', 'Fe': 'Железо', 'O': 'Кислород',
                          'Na': 'Натрий', 'K': 'Калий', 'N': 'Азот', 'Mg': 'Магний', 'Ca': 'Кальций',
                          'Zn': 'Цинк', 'Ag': 'Серебро', 'Au': 'Золото', 'Al': 'Алюминий', 'Si': 'Кремний'}
@@ -306,6 +289,8 @@ class HardElem(QWidget):
         # Window setup
         self.setGeometry(412, 412, 412, 500)
         self.setWindowTitle('Хiмоz@ for punks')
+        self.setStyleSheet("background-color:" + choice(['red', 'green', 'white', 'yellow', 'grey', 'purple', 'orange',
+                                                         'brown', 'pink', 'blue', 'olive']))
         self.setWindowIcon(QIcon("icon.jpg"))
 
         self.hardelem = {'H': 'Водород', 'Li': 'Литий', 'Cu': 'Медь', 'Fe': 'Железо', 'O': 'Кислород',
@@ -370,38 +355,6 @@ class HardElem(QWidget):
 
     def incorrect(self):
         # Form changer
-        self.close()
-
-
-class Wait(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        # Window setup
-        self.setGeometry(412, 412, 412, 500)
-        self.setWindowTitle('Хiмоz@ for punks')
-        self.pixmap = QPixmap('ximoza.jpg')
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(412, 412)
-        self.image.setPixmap(self.pixmap)
-        self.setWindowIcon(QIcon("icon.jpg"))
-
-        # Button setup
-        self.btn = QPushButton('Хочу продолжения', self)
-        self.btn.resize(412, 100)
-        self.btn.move(0, 400)
-        self.btn.clicked.connect(self.open_second_form)
-        self.name_label = QLabel(self)
-        self.name_label.setText('В разработке')
-        self.name_label.move(40, 90)
-
-    def open_second_form(self):
-        # Form changer
-        self.second_form = Menu(self, "")
-        self.second_form.show()
         self.close()
 
 
